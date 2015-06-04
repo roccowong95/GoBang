feaf19072354607e58313f7ae11bee70dae6269f
Rocco <roccowang95@gmail.com>
2015-06-05 00:42:58 +0800

    现在用6分支4层,可以下满棋盘.下一步准备修改分值表

--- src/blackclient/config1.java
+++ src/blackclient/config1.java
@@ -21,6 +21,6 @@ public class config1 {
     public static byte BLACK = 1;
     public static byte WHITE = -1;
     public static int REP=1;//test
-    public static int N = 5;//branch of the game tree
+    public static int N = 6;//branch of the game tree
     public static int Depth = 4;//depth of game tree
 }
